bb71b32d58547a3083497f608fd217b8a89e395e
valerygordeev <valerygordeev@gmail.com>
2025-05-16 15:22:18 +0300

    test1

--- main.py
+++ main.py
@@ -1,22 +1,68 @@
-NUM_DAYS = 5
+
+class Human:
+    def __init__(self,name):
+        self.name=name
+
+    def answer_question(self):
+        print('Очень интересныей вопрос! Не знаю.')
+
+class Student(Human):
+    def __init__(self,name):
+        super().__init__(name)
+
+    def ask_question(self,Human,question):
+        print(f'{Human.name}, {question}')
+
+    def answer_question(self):
+      print(f'Сейчас расскажу.')
+
+class Mentor(Human):
+    def __init__(self,name):
+        super().__init__(name)
+
+    def answer_question(self):
+      print('Отдохни и возвращайся с вопросами по теории.')
+
+class CodeReviewer(Human):
+    def __init__(self,name):
+        super().__init__(name)
+
+    def answer_question(self):
+      print('О, вопрос про проект, это я люблю.')
 
 
-def main():
-    sales = [0] * NUM_DAYS
+class Curator(Human):
+    def __init__(self,name):
+        super().__init__(name)
 
-    print("Введите продажи за каждый день")
+    def answer_question(self):
+      print('Держись, всё получится. Хочешь видео с котиками?')
 
-    for index in range(len(sales)):
-        sales[index] = float(input(f'День # {index + 1}: '))
 
-    print("Введенные значения: ")
-
-    for value in sales:
-        print(value)
-
-    print("\nИТОГО:")
-    print(sum(sales))
-
+student=Student('Ира')
+mentor=Mentor('Ира')
+codereviewer=CodeReviewer('Евгений')
+curator=Curator('Марина')
+print('----------------------')
+student.ask_question(curator,'мне грустненько, что делать?')
+curator.answer_question()
+print()
+student.ask_question(mentor,'мне грустненько, что делать?')
+mentor.answer_question()
+print()
+human=Human('Евгений')
+student.ask_question(human,'когда каникулы?')
+human.answer_question()
+print()
+student.ask_question(codereviewer,'что не так с моим проектом?')
+codereviewer.answer_question()
+print()
+human=Human('Виталя')
+student.ask_question(human,'как устроиться на работу питонистом?')
+human.answer_question()
+print()
+student.ask_question(student,'как устроиться работать питонистом?')
+student.answer_question()
 
 if __name__ == '__main__':
     main()
